6ac8261347de92a10fb0e45af7730f95f5e8b692
chonlakan <chonrakan56@gmail.com>
2016-06-13 10:29:05 +0700

    Start project 13/06/2016

---
+++ app/src/androidTest/java/pbru/chonlakan/yaemsak/pbru_run/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pbru.chonlakan.yaemsak.pbru_run;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MainActivity.java
@@ -0,0 +1,23 @@
+package pbru.chonlakan.yaemsak.pbru_run;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }//Main method
+
+
+
+}//Main Class

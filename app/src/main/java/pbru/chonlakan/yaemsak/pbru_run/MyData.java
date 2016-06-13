612a68d6755863e10676df7b074f6d6b9045fce8
chonlakan <chonrakan56@gmail.com>
2016-06-13 16:53:13 +0700

    Finish Day1

---
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MyData.java
@@ -0,0 +1,13 @@
+package pbru.chonlakan.yaemsak.pbru_run;
+
+
+public class MyData {
+    //Explicit
+    private int[] avatarInts = new int[]{R.drawable.bird48, R.drawable.doremon48,
+                        R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
+
+    public int[] getAvatarInts() {//command + N .Getter
+
+        return avatarInts;
+    }
+}//Main Class
--- app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/SignUpActivity.java
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/SignUpActivity.java
@@ -2,15 +2,102 @@ package pbru.chonlakan.yaemsak.pbru_run;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avatar0RadioButton , avatar1RadioButton, avatar2RadioButton,
+                        avatar3RadioButton, avatar4RadioButton;
+    private String nameString, userString, passwordString, avatarString;
+    private int indexAnInt = 0;
+
+    //http://swiftcodingthai.com/pbru3/add_user_master.php
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //BindWidget
+        bindWidget();
+
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (indexAnInt) {
+                    case R.id.radioButton:
+                        indexAnInt = 0;
+                        break;
+                    case R.id.radioButton2:
+                        indexAnInt = 1;
+                        break;
+                    case R.id.radioButton3:
+                        indexAnInt = 2;
+                        break;
+                    case R.id.radioButton4:
+                        indexAnInt = 3;
+                        break;
+                    case  R.id.radioButton5:
+                        indexAnInt = 4;
+                        break;
+                }
+
+            }
+        });
+
     }//Main Method
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvatar);
+        avatar0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avatar1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avatar2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avatar3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avatar4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }//BindWidget
+
+    public void clickSignUpSign(View view) {
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            Toast.makeText(this, "มีช่องว่าง กรุณากรอกทุกช่อง", Toast.LENGTH_SHORT).show();
+
+        } else {
+            //No Space
+            uploadValueToServer();
+        }
+
+
+
+    }//clickSignUpSign
+
+    private void uploadValueToServer() {
+
+        Log.d("pbruV1" , "Name ==>" + nameString);
+        Log.d("pbruV1" , "User ==>" + userString);
+        Log.d("pbruV1" , "Pass ==>" + passwordString);
+        Log.d("pbruV1" , "Avatar ==>" + Integer.toString(indexAnInt));
+
+    }//UpLoadToServer
 
 }//Main Class

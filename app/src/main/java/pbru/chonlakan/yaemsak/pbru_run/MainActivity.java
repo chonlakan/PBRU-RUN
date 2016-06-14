f38cb4b997977363d64eb9c2b38e9a78db4eb70d
chonlakan <chonrakan56@gmail.com>
2016-06-14 15:51:23 +0700

    Start Connect to server

--- app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MainActivity.java
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MainActivity.java
@@ -1,15 +1,26 @@
 package pbru.chonlakan.yaemsak.pbru_run;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
-
+    private static final String urlJSON = "http://swiftcodingthai.com/pbru3/get_user.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,76 @@ public class MainActivity extends AppCompatActivity {
         //Request SQLite เพื่อสร้าง SQLite Database
         myManage = new MyManage(this);
 
+        deleteDatabase();
+
+        //สั่งให้ trade ทำงาน
+        ConnectedServer connectedServer = new ConnectedServer();
+        connectedServer.execute();
+
 
     }//Main method
+
+    private class ConnectedServer extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            try{
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            }catch (Exception e){
+                return null;
+            }
+        }//doIn back
+
+        @Override //ทำงานหลังโหลด
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("pbruV2", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i=0; i<jsonArray.length(); i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strId = jsonObject.getString("id");
+                    String strName = jsonObject.getString("Name");
+                    String strUser= jsonObject.getString("User");
+                    String strPassword = jsonObject.getString("Password");
+                    String strAvata = jsonObject.getString("Avata");
+                    String strGold = jsonObject.getString("Gold");
+
+                    myManage.addNewUser(strId, strName, strUser, strPassword, strAvata, strGold);
+
+                }//for
+
+                SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name, MODE_PRIVATE, null);
+                Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE", null);
+                cursor.moveToFirst();
+                Log.d("pbruV3", "cursor == >" + cursor.getCount());
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }//onPostEx
+
+    }//Connected
+
+
+    private void deleteDatabase() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,MODE_PRIVATE,null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+    }
 
 
     public void clickSignUpMain(View view) {
--- app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MyManage.java
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MyManage.java
@@ -1,17 +1,24 @@
 package pbru.chonlakan.yaemsak.pbru_run;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by lap324-17 on 6/14/2016 AD.
- */
+
 public class MyManage {
 
     //Explicit
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_name = "Name";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_avata = "Avata";
+    public static final String column_gold = "Gold";
 
 
     public MyManage(Context context) {
@@ -20,4 +27,22 @@ public class MyManage {
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }//Constructor
+
+    public long addNewUser(String strId,
+                           String strName,
+                           String strUser,
+                           String strPassword,
+                           String strAvata,
+                           String strGold) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_id, strId);
+        contentValues.put(column_name, strName);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+        contentValues.put(column_avata, strAvata);
+        contentValues.put(column_gold, strGold);
+
+
+        return sqLiteDatabase.insert(user_table,null, contentValues);
+    }
 }//Main Class

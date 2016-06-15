4a6a8078f05e85ba6f4b3b3afd8bdf8fd039f066
chonlakan <chonrakan56@gmail.com>
2016-06-15 14:28:00 +0700

    Create map finish

--- app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MapsActivity.java
+++ app/src/main/java/pbru/chonlakan/yaemsak/pbru_run/MapsActivity.java
@@ -1,7 +1,13 @@
 package pbru.chonlakan.yaemsak.pbru_run;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,17 +18,104 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //Explicit
     private GoogleMap mMap;
+    private double userLatADouble, userLngADouble;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private String[] userLoginStrings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        userLoginStrings = getIntent().getStringArrayExtra("Login");
+
+        //Setup Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);//open service
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);//close altitude Z
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }//Main method
+
+    @Override
+    protected void onResume() {//ทำหลังหยุดไป
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+
+        Location fromISPLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);//ถามไปที่เครือข่ายของผู้ให้บริการ
+        if (fromISPLocation != null) {
+
+            userLatADouble = fromISPLocation.getLatitude();
+            userLngADouble = fromISPLocation.getLongitude();
+
+        }
+
+        Location fromGPSLocation = myFindLocation(LocationManager.GPS_PROVIDER);//card GPS
+        if (fromGPSLocation != null) {
+
+            userLatADouble = fromGPSLocation.getLatitude();
+            userLngADouble = fromGPSLocation.getLongitude();
+
+        }
+
+        Log.d("pbruV5", "UserLat ==> " + userLatADouble);
+        Log.d("pbruV5", "UserLng ==> " + userLngADouble);
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("pbruV4", "Cannot find location");
+        }
+
+        return location;
+    }//myFindLocation
+
+
+    public final LocationListener locationListener = new LocationListener() { //android.location
+        @Override
+        public void onLocationChanged(Location location) {
+            userLatADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+
+        }//onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -32,5 +125,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }//onMapReady
+
+}//Main Class
